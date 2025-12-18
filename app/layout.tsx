dbda76b66459434125e5f18bec18db2015f02b4b
Timothy Onoja <timothyonoja10@gmail.com>
2025-12-18 09:36:50 +0100

    home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer"
+import type { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children } : {children: ReactNode}) {
   return (
     <html lang="en">
       <body>
